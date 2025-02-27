e054f780fa80d5b4c821799174a032afea5012e9
Jan Zyśko <janzysko@gmail.com>
2025-02-27 18:35:57 +0100

    Update models.py

--- jobscraper/scraper/models.py
+++ jobscraper/scraper/models.py
@@ -11,6 +11,44 @@ class JobBoard(models.IntegerChoices):
     JUST_JOIN_IT = 2
     PRACUJ = 3
 
+class CompanyManager(models.Manager):
+    def get_possible_match(self, name: str) -> models.QuerySet:
+        """There are different company names on different boards."""
+        # TODO: matching by company url
+        # TODO: non-polish: gmbh, llc
+        stripped_name = name.replace('sp. z o.o.', '').strip()
+        # if name didn't have sp. z o.o., let's add it to the check
+        name_with_sp = f'{stripped_name} sp. z o.o.'
+        return (
+            self.filter(name__iexact=stripped_name)
+            | self.filter(name__iexact=name_with_sp)
+        )
+        
+    def create_or_update_if_better(
+        self,
+        name: str,
+        url: str,
+        **kwargs,
+    ):
+        url = url.strip()
+        possible_matches = self.get_possible_match(name)
+        if possible_matches.count() == 1:
+            # if we have only one possible match, let's update it
+            company = possible_matches.first()
+            # Update company if needed
+            if company.url != url:
+                company.url = url
+                company.save(update_fields=['url'])
+            return company
+        # if 0 or more than 1 possible matches, we create a new company
+        # if there's more than 1, we can't be sure that this is the same one,
+        # so we create a new company
+        return self.create(
+            name=name.replace('sp. z o.o.', '').strip(),
+            url=url,
+            **kwargs,
+        )
+
 class Company(models.Model):
     name = models.CharField(max_length=255)
     industry = models.CharField(max_length=255, blank=True)
@@ -18,13 +56,14 @@ class Company(models.Model):
     size_to = models.IntegerField(default=0)
     url = models.CharField(max_length=1024, blank=True)
     status = models.IntegerField(choices=HypeStatus.choices, default=HypeStatus.UNKNOWN)
+    
+    objects = CompanyManager()
 
     def __str__(self) -> str:
         return self.name
         
     class Meta:
         db_table = 'grabbo_company'  # Use existing table
-        unique_together = ['name'] 
 
 class Job(models.Model):
     board = models.IntegerField(choices=JobBoard.choices)
@@ -37,6 +76,9 @@ class Job(models.Model):
     status = models.IntegerField(choices=HypeStatus.choices, default=HypeStatus.UNKNOWN)
     created_at = models.DateTimeField(auto_now_add=True)
     lena_comparibility = models.FloatField(default=0.0)
+    # Adding fields that are required in the database but were missing in your model
+    description = models.TextField(null=True, blank=True)  # Required field
+    # Add any other missing fields here
 
     def __str__(self) -> str:
         return f'{self.title} in {self.company}'
